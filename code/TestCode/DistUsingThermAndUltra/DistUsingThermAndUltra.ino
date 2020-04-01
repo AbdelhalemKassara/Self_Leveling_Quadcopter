f633e33e6fe13be40b047d3ad42b85295e4225a3
Abdelhalem Kassara <abdelhalemkassara@yahoo.com>
2020-04-01 18:12:08 -0400

    finished the code for getting distance but need to make it a bit more efficent

---
+++ code/TestCode/DistUsingThermAndUltra/DistUsingThermAndUltra.ino
@@ -0,0 +1,31 @@
+#define TRIG_PIN 4
+#define ECHO_PIN 5
+
+void setup() {
+  Serial.begin(2000000);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+}
+
+void loop() {
+  Serial.print(GetDistance());
+  Serial.println("cm");
+}
+
+float GetDistance() {
+  digitalWrite(TRIG_PIN, LOW);          // This sets the trigger pin to low or 0v to make sure that the pin is not sending out a a signal
+  delayMicroseconds(2);                 // This pauses the program for 2 microseconds
+  digitalWrite(TRIG_PIN, HIGH);         // This sets the trigger pin to high or 5v to send out a signal
+  digitalWrite(TRIG_PIN, LOW);          // This sets the trigger pin to low or 0v to finish the pulse
+  float Time = pulseIn (ECHO_PIN, HIGH);   // This returns the time starting from when the TrigPin went high to when the Echo pin goes high and makes it equal to Time
+  Time /= 2000000; //converts the time from microseconds to seconds and divides the time by 2 (100000*2) because half dist
+
+  double tempC;
+  int tempReading;
+
+  tempReading = analogRead(0);    // This gets the voltage back in the range of 0 to 1023 //the pin for the thermistor is 0
+  tempC = log(10000.0 * ((1024.0 / tempReading - 1)));    //converts the voltage to Kelvin
+  tempC = (1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempC * tempC )) * tempC )) - 273.15;//converts voltage to kelvin then to celsius
+Serial.println(tempC);
+  return ((331.5 + 0.6 * tempC) * Time) * 100;
+}
